4aba6dda6398c1c32df1dea2fecd220f42b2699a
Kristen M. Thyng <kthyng@gmail.com>
2013-06-25 11:19:04 -0500

    my version of the files

--- sw_engineering/test_animals.py
+++ sw_engineering/test_animals.py
@@ -2,4 +2,15 @@ import animals
 
 
 def test_read_animals():
-    pass
+	ref_dates = ['2011-04-22', '2011-04-23', '2011-04-23', '2011-04-23', '2011-04-23']
+	ref_times = ['21:06', '14:12', '10:24', '20:08', '18:46']
+	ref_species = ['Grizzly', 'Elk', 'Elk', 'Wolverine', 'Muskox']
+	ref_counts = [36, 25, 26, 31, 20]
+
+	dates, times, species, counts = animals.read_animals('animals.txt')
+
+	assert dates == ref_dates
+	assert times == ref_times
+	assert counts == ref_counts
+	assert species == ref_species
+    
